74d9df1dfb63749c0ae072ab1cd67ff8ce9fe270
Mark Farmiloe <mark@ccsdev.net>
2024-03-18 18:33:49 +0000

    Add sorting by clicking Title

--- src/components/DTHTitle.jsx
+++ src/components/DTHTitle.jsx
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types';
 
-const DTHTitle = ({fieldInfo}) => {
+const DTHTitle = ({fieldInfo, changeSort}) => {
     const {fldno, text, imgsrc, sortIndex} = fieldInfo;
-    // console.log(fldno, text, imgsrc, sortIndex);
+    const handleClick = (e) => {
+        const asc = !e.shiftKey;
+        const append = e.ctrlKey;
+        changeSort(append, fldno, asc);
+    }
     return (
-        <th scope="col" value={fldno}>
+        <th scope="col" value={fldno} onClick={handleClick}>            
             {text}
-            { sortIndex > 0 && 
-                <> <img src={imgsrc}/><span>{1 + sortIndex}</span></>
+            { sortIndex >= 0 && 
+                <> <img  src={imgsrc} /><span>{1 + sortIndex}</span></>
             }
         </th>
     )
@@ -18,7 +22,8 @@ DTHTitle.propTypes = {
         text: PropTypes.string.isRequired,
         imgsrc: PropTypes.string,
         sortIndex: PropTypes.number
-    })
+    }).isRequired,
+    changeSort: PropTypes.func.isRequired
 }
 
 export default DTHTitle;
